19d8388e9343132e31f74c7044f56d7baa5ff326
Damien Lebrun <dinoboff@hotmail.com>
2014-01-09 18:47:32 +0000

    add test for HomeCtrl.

--- test/unit/homePagesSpec.js
+++ test/unit/homePagesSpec.js
@@ -7,17 +7,46 @@ describe('Home Pages', function() {
   beforeEach(module('app.homePages'));
 
   describe('Home Menu', function(){
-    beforeEach(inject(function($controller, $rootScope) {
+    var files, allDefer;
+
+    beforeEach(inject(function($controller, $rootScope, $q) {
       scope = $rootScope.$new();
+      allDefer = $q.defer();
+
+      files = {'all': function(){
+          return allDefer.promise;
+        }
+      };
+
       ctrl = $controller('HomeCtrl', {
         $scope: scope,
+        files: files
       });
     }));
 
     describe('Initialization', function(){
       it('should set the list of the current user list of file', function() {
         expect(scope.files).toEqual([]);
+        expect(scope.loading).toBe(true);
       });
+    });
+
+    describe('files scope attribute', function(){
+
+      it('should update when files.all() promise is resolved', function(){
+        var data = [
+          {
+            'name': 'foo',
+            'lastUpdate': 1234567890,
+            'key': 'foo'
+          }
+        ];
+
+        allDefer.resolve(data);
+        scope.$apply();
+        expect(scope.files).toEqual(data);
+      });
+
     });
 
   });
